b19eb0b9b925a50c49d337f61b97cc096d601b8a
RamezCh <ramiz_chreide@outlook.com>
2024-07-15 18:53:00 +0300

    Controlled Elements & New Items

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import { useState } from 'react';
+
 const initialItems = [
   { id: 1, description: 'Passports', quantity: 2, packed: true },
   { id: 2, description: 'Hiking Boots', quantity: 1, packed: false },
@@ -21,17 +23,47 @@ function Logo() {
 }
 
 function Form() {
+  const [description, setDescription] = useState('');
+  const [quantity, setQuantity] = useState(1);
+  // Controlled Element steps:
+  // 1_ Define the state like quantity above
+  // 2_ Use the state on the element we want to controll
+  // 3_ Update that state variable with the onChange event
+  function handleSubmit(e) {
+    e.preventDefault();
+    // Check if Empty
+    if (!description.trim()) return;
+    // Create new item
+    const newItem = {
+      description,
+      quantity,
+      packed: false,
+      id: Date.now(),
+    };
+    // Return to Initial State
+    setDescription('');
+    setQuantity(1);
+  }
+
   return (
-    <form className="add-form">
+    <form className="add-form" onSubmit={handleSubmit}>
       <h3>What do you need for your 😍 trip?</h3>
-      <select>
+      <select
+        value={quantity}
+        onChange={e => setQuantity(Number(e.target.value))}
+      >
         {Array.from({ length: 20 }, (_, i) => i + 1).map(num => (
           <option value={num} key={num}>
             {num}
           </option>
         ))}
       </select>
-      <input type="text" placeholder="Item..." />
+      <input
+        type="text"
+        placeholder="Item..."
+        value={description}
+        onChange={e => setDescription(e.target.value)}
+      />
       <button>Add</button>
     </form>
   );
