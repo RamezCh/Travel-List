4e8abfe3f6d3dab4ebd1b13c1fc6ca4c30016a11
RamezCh <ramiz_chreide@outlook.com>
2024-07-15 16:18:53 +0300

    Item Structure

--- src/App.js
+++ src/App.js
@@ -1,3 +1,10 @@
+const initialItems = [
+  { id: 1, description: 'Passports', quantity: 2, packed: true },
+  { id: 2, description: 'Hiking Boots', quantity: 1, packed: false },
+  { id: 3, description: 'Sleeping Bag', quantity: 1, packed: false },
+  { id: 4, description: 'Socks', quantity: 12, packed: false },
+];
+
 export default function App() {
   return (
     <div className="app">
@@ -22,12 +29,32 @@ function Form() {
 }
 
 function PackingList() {
-  return <div className="list">LIST</div>;
+  return (
+    <div className="list">
+      <ul>
+        {initialItems.map(item => (
+          <Item item={item} />
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+function Item({ item }) {
+  return (
+    <li key={item.id}>
+      <input type="checkbox" />
+      <span style={item.packed ? { textDecoration: 'line-through' } : {}}>
+        {item.quantity}x {item.description}
+      </span>
+      <button>❌</button>
+    </li>
+  );
 }
 
 function Stats() {
   return (
-    <footer>
+    <footer className="stats">
       <em>You have ? items on your list, and you already packed ? (?%)</em>
     </footer>
   );
