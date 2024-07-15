2a33e6ec4efb36152955725b6a30e1272093fb52
RamezCh <ramiz_chreide@outlook.com>
2024-07-15 15:57:40 +0300

    Initial Layout

--- src/App.js
+++ src/App.js
@@ -1,3 +1,34 @@
 export default function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="app">
+      <Logo />
+      <Form />
+      <PackingList />
+      <Stats />
+    </div>
+  );
+}
+
+function Logo() {
+  return <h1>🏝 Far Away 🧳</h1>;
+}
+
+function Form() {
+  return (
+    <div className="add-form">
+      <h3>What do you need for your 😍 trip?</h3>
+    </div>
+  );
+}
+
+function PackingList() {
+  return <div className="list">LIST</div>;
+}
+
+function Stats() {
+  return (
+    <footer>
+      <em>You have ? items on your list, and you already packed ? (?%)</em>
+    </footer>
+  );
 }
