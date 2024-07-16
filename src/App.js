2eac824b77caec2a3a58b7c36aabe1107981f79a
RamezCh <ramiz_chreide@outlook.com>
2024-07-16 14:26:42 +0300

    Implement Sort Feature

--- src/App.js
+++ src/App.js
@@ -95,10 +95,26 @@ function Form({ onAddItems }) {
 }
 
 function PackingList({ items, onDeleteItem, onToggleItem }) {
+  const [sortBy, setSortBy] = useState('input');
+
+  let sortedItems;
+
+  if (sortBy === 'input') sortedItems = items;
+
+  if (sortBy === 'description')
+    sortedItems = items
+      .slice()
+      .sort((a, b) => a.description.localeCompare(b.description));
+
+  if (sortBy === 'packed')
+    sortedItems = items
+      .slice()
+      .sort((a, b) => Number(a.packed) - Number(b.packed));
+
   return (
     <div className="list">
       <ul>
-        {items.map(item => (
+        {sortedItems.map(item => (
           <Item
             key={item.id}
             item={item}
@@ -107,6 +123,14 @@ function PackingList({ items, onDeleteItem, onToggleItem }) {
           />
         ))}
       </ul>
+
+      <div className="actions">
+        <select value={sortBy} onChange={e => setSortBy(e.target.value)}>
+          <option value="input">Sort by input order</option>
+          <option value="description">Sort by description</option>
+          <option value="packed">Sort by packed status</option>
+        </select>
+      </div>
     </div>
   );
 }
@@ -128,17 +152,17 @@ function Item({ item, onDeleteItem, onToggleItem }) {
 }
 
 function Stats({ items }) {
-  const packedItems = items.filter(item => item.packed);
-  const percentageOfPackedItems = Math.round(
-    (packedItems.length / items.length) * 100
-  );
-
   if (!items.length)
     return (
       <p className="stats">
         <em>Start adding some items to your packing list 🚀</em>
       </p>
     );
+
+  const packedItems = items.filter(item => item.packed);
+  const percentageOfPackedItems = Math.round(
+    (packedItems.length / items.length) * 100
+  );
 
   return (
     <footer className="stats">
